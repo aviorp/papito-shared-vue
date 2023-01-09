7c42b8463160c638db56b6195899c39f8c46e208
Avior Peretz <avior.peretz@aquasec.com>
2023-01-09 22:04:57 +0200

    0.1.20

--- src/imports.js
+++ src/imports.js
@@ -5,15 +5,3 @@ export { default as PDrawer } from "./components/PDrawer.vue";
 export { default as PInput } from "./components/PInput.vue";
 export { default as PModal } from "./components/PModal.vue";
 export { default as PPage } from "./layouts/PageLayout.vue";
-
-export const PAPITO_DAISY_CONFIGURATIONS = {
-  primary: "#4B6BFB",
-  secondary: "#7B92B2",
-  accent: "#67CBA0",
-  neutral: "#181A2A",
-  "base-100": "#FFFFFF",
-  info: "#3ABFF8",
-  success: "#36D399",
-  warning: "#FBBD23",
-  error: "#F87272",
-};
