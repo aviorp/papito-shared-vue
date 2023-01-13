2e4c7723e92f4b9b47529744c40fe5edacc176ca
Avior Peretz <avior.peretz@aquasec.com>
2023-01-13 19:42:47 +0200

    0.1.24

--- src/imports.js
+++ src/imports.js
@@ -2,6 +2,7 @@ export { default as PBreadcrumbs } from "./components/PBreadcrumbs.vue";
 export { default as PButton } from "./components/PButton.vue";
 export { default as PCard } from "./components/PCard.vue";
 export { default as PDrawer } from "./components/PDrawer.vue";
+export { default as PDropdown } from "./components/PDropdown.vue";
 export { default as PInput } from "./components/PInput.vue";
 export { default as PLoading } from "./components/PLoading.vue";
 export { default as PModal } from "./components/PModal.vue";
